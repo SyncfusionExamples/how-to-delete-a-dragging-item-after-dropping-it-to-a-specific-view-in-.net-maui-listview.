48023262013800586d896089271e9a811a5671b6
AnanthaLakshmiKannan <ananthalakshmi.k@syncfusion.com>
2023-06-16 19:17:27 +0530

    831881: Sample Added for delete-a-dragging-item-after-dropping-it-to-a-specific-view-in-.net-maui-listview.

--- ListViewMaui/Converters/InverseBoolConverter.cs
+++ ListViewMaui/Converters/InverseBoolConverter.cs
@@ -8,15 +8,7 @@ using System.Threading.Tasks;
 namespace ListViewMaui
 {
     public class InverseBoolConverter : IValueConverter
-    {
-        /// <summary>
-        /// Convert either true or false.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <param name="targetType">The TargetType.</param>
-        /// <param name="parameter">The converter parameter.</param>
-        /// <param name="culture">The culture info.</param>
-        /// <returns>Returns the boolean value based on the CultureInfo.</returns>
+    { 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is bool)
@@ -26,15 +18,7 @@ namespace ListViewMaui
 
             return true;
         }
-
-        /// <summary>
-        /// Convert either true or false.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <param name="targetType">The TargetType.</param>
-        /// <param name="parameter">The converter parameter.</param>
-        /// <param name="culture">The culture info.</param>
-        /// <returns>Returns the boolean value.</returns>
+       
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return value;
